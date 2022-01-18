52be511665f27a27f396ed9f752b925764880385
Matt Foster <fostmatt@Matts-MacBook-Pro.local>
2022-01-17 19:36:06 -0700

    add schema file

--- server.js
+++ server.js
@@ -14,13 +14,35 @@ inquirer.prompt([
     }
 ])
 .then(answers => {
-    console.log('hi');
+   switch (answers.choiceSelect) {
+        case 'View all departments':
+            viewDepartments();
+            break;
+        case 'View all roles':
+            viewRoles();
+            break;
+        case 'View all employees':
+            viewEmployees();
+            break;
+        case 'Add a department':
+            addDepartment();
+            break;
+        case 'Add a role':
+            addRole();
+            break; 
+        case 'Add an employee':
+            addEmployee();
+            break; 
+        case 'Update an employee role':
+            updateEmployee();
+            break; 
+   }
 })
 };
 promptUser();
 
-function viewDepartments() {
-    console.log('successful!')
+function viewDepartments(){
+    
 }
 
 
